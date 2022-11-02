841f901d4dbd00313bbe88a07567c487b8e1c31f
grozny <h6umid@gmail.com>
2022-11-02 10:15:19 +0300

    создал объект

---
+++ soldier.js
@@ -0,0 +1,18 @@
+const soldier = {
+    name: 'Alpha',
+    hp: 10,
+    weapon: {
+        weaponName: 'ak47',
+        cartridges: 30,
+    },
+    supplies: 3,
+    fire() {
+
+    },
+    recharge() {
+
+    },
+    hurt() {
+
+    },
+}
