16056de5127f522dd1f2e97c2254f038b6ffb57f
grozny <h6umid@gmail.com>
2022-11-02 11:09:20 +0300

    Написал свою первую программу для игры

--- soldier.js
+++ soldier.js
@@ -3,16 +3,28 @@ const soldier = {
     hp: 10,
     weapon: {
         weaponName: 'ak47',
-        cartridges: 30,
+        cartridges: 0,
     },
-    supplies: 3,
+    supplies: 1,
     fire() {
-
+        this.weapon.cartridges--
+        console.log('б1ов-б1ов')
+        if (this.weapon.cartridges === 0) {
+            console.log('Обойма пуста. Перезарядитесь.')
+        }
     },
     recharge() {
-
+      this.weapon.cartridges = 30
+      this.supplies--
+      console.log('перезарядка...')
+      if (this.supplies === 0) {
+        console.log('Не осталоссь боеприпасов.')
+      }
     },
     hurt() {
-
+      this.hp--
+      if(this.hp === 0) {
+        console.log('Ты проиграл')
+      }
     },
 }
